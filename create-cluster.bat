e38c3dcb648b33183565bb44d52e5e9710b0f648
choresh <39343045+choresh@users.noreply.github.com>
2020-10-08 23:10:51 +0300

    Successful build. with temp app.

--- create-cluster.bat
+++ create-cluster.bat
@@ -1,12 +1,13 @@
 @ECHO OFF
 
-REM 1st part of the comands based on: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-cli-tutorial-ec2.html.
-REM 2nd part of the comands (from 'Get Repository info - started') based on: https://medium.com/javascript-in-plain-english/deploy-your-node-app-to-aws-container-service-via-github-actions-build-a-pipeline-c114adeb8903.
+REM 1st and 3rd parts of the comands based on: https://medium.com/javascript-in-plain-english/deploy-your-node-app-to-aws-container-service-via-github-actions-build-a-pipeline-c114adeb8903.
+REM 2nd part of the comands based on: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-cli-tutorial-ec2.html.
 REM More info - see the 'README.md' file of this package.
 
 SET AWS_ACCESS_KEY_ID=
 SET AWS_SECRET_ACCESS_KEY=
-SET CLUSTER_SUFFIX=100
+SET APP_NAME=aws-deployment-test
+SET RESOURCES_SUFFIX=1
 SET REGION=us-east-2
 REM TODO: 8080
 SET PORT=80
@@ -31,16 +32,64 @@ SET ECS_PARAMS_FILE_NAME=%AWS_TEMP_FOLDER%ecs-params.yml
 SET GITHUB_PARAMS_TEMPLATE_FILE_NAME=%GITHUB_WORKFLOWS_FOLDER%deploy-to-aws-template.yml
 SET GITHUB_PARAMS_FILE_NAME=%GITHUB_WORKFLOWS_FOLDER%deploy-to-aws.yml
 SET MY_UTILS_PATH="%~dp0build/aws/src/create-cluster-utils.js"
-SET ROLE_NAME=ecsTaskExecutionRole-%CLUSTER_SUFFIX%
-SET PROFILE_NAME=profile-%CLUSTER_SUFFIX%
-SET CLUSTER_NAME=cluster-%CLUSTER_SUFFIX%
-SET STACK_NAME=amazon-ecs-cli-setup-%CLUSTER_SUFFIX%
-SET REPOSITORY_NAME=%CLUSTER_NAME%
+SET ROLE_NAME=ecsTaskExecutionRole-%RESOURCES_SUFFIX%
+SET PROFILE_NAME=profile-%RESOURCES_SUFFIX%
+SET STACK_NAME=amazon-ecs-cli-setup-%RESOURCES_SUFFIX%
+
+SET REPOSITORY_NAME=%APP_NAME%
+SET CLUSTER_NAME=%APP_NAME%
+SET ECR_REPOSITORY=%APP_NAME%
+SET SERVICE_NAME=%APP_NAME%
+SET CONTAINER_NAME=%APP_NAME%
 
 ECHO Clear all resources (if exists) - started
-ecs-cli compose --project-name %CLUSTER_NAME% service down --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
-ecs-cli down --force --cluster-config %CLUSTER_NAME% s
-ECHO Clear all resources if exists) - ended
+REM ecs-cli compose --project-name %APP_NAME% service down --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME%
+REM ecs-cli down --force --cluster-config %APP_NAME% s
+ECHO Clear all resources (if exists) - ended
+
+REM ================= 1st part - start ==============================
+
+ECHO Get Repository info - started
+aws ecr describe-repositories --repository-names %REPOSITORY_NAME% --region %REGION% > %REPOSITORY_INFO_FILE_NAME%
+ECHO Get Repository info - ended
+
+ECHO Fetch Repository info - started
+CALL node %MY_UTILS_PATH% --repository-info %REPOSITORY_INFO_FILE_NAME% %REPOSITORY_INFO_PROCESSED_FILE_NAME%
+FOR /f "tokens=1,2 delims==" %%A in (%REPOSITORY_INFO_PROCESSED_FILE_NAME%) do (
+    SET FOUND_%%A=%%B
+)
+ECHO Fetch Repository info - ended
+
+IF NOT DEFINED FOUND_REPOSITORY_URI (
+    ECHO Create repository - started
+    aws ecr create-repository --repository-name %REPOSITORY_NAME% --region %REGION% > %REPOSITORY_INFO_FILE_NAME%
+    ECHO Create repository - ended 
+)
+
+ECHO Fetch Repository info (2nd time) - started
+CALL node %MY_UTILS_PATH% --repository-info %REPOSITORY_INFO_FILE_NAME% %REPOSITORY_INFO_PROCESSED_FILE_NAME%
+FOR /f "tokens=1,2 delims==" %%A in (%REPOSITORY_INFO_PROCESSED_FILE_NAME%) do (
+    SET FOUND_%%A=%%B
+)
+ECHO Fetch Repository info (2nd time) - ended
+
+ECHO REPOSITORY_URI: '%FOUND_REPOSITORY_URI%'
+
+ECHO Build - started
+REM TODO: add tag!
+REM docker build -t %FOUND_REPOSITORY_URI% .
+ECHO Build - ended
+
+ECHO Push - started
+REM TODO: add tag!
+REM docker push %FOUND_REPOSITORY_URI%
+ECHO Push - ended
+PAUSE
+
+REM ================= 1st part - end ==============================
+
+
+REM ================= 2nd part - start ==============================
 
 ECHO Create role (if not exists) - sarted
 aws iam --region %REGION% create-role --role-name %ROLE_NAME% --assume-role-policy-document file://%ROLE_POLICY_FILE% > %ROLE_INFO_FILE_NAME%
@@ -55,11 +104,11 @@ ecs-cli configure profile --access-key %AWS_ACCESS_KEY_ID% --secret-key %AWS_SEC
 ECHO Create ECS CLI profile - ended
 
 ECHO Create cluster configuration - started
-ecs-cli configure --cluster %CLUSTER_NAME% --default-launch-type FARGATE --config-name %CLUSTER_NAME% --region %REGION%
+ecs-cli configure --cluster %APP_NAME% --default-launch-type FARGATE --config-name %APP_NAME% --region %REGION%
 ECHO Create cluster configuration - ended
 
 ECHO Create cluster - started
-ecs-cli up --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME% > %VPC_INFO_FILE_NAME% --force
+ecs-cli up --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME% > %VPC_INFO_FILE_NAME% --force
 ECHO Create cluster - ended
 
 ECHO Fetch VPC info - started
@@ -93,11 +142,11 @@ CALL node %MY_UTILS_PATH% --ecs-params %ECS_PARAMS_TEMPLATE_FILE_NAME% %ECS_PARA
 ECHO Set ESC params - ended
 
 ECHO Deploy the compose file to the cluster - started
-ecs-cli compose --ecs-params %ECS_PARAMS_FILE_NAME% --project-name %CLUSTER_NAME% service up --create-log-groups --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
+ecs-cli compose --ecs-params %ECS_PARAMS_FILE_NAME% --project-name %APP_NAME% service up --create-log-groups --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME%
 ECHO Deploy the compose file to the cluster - ended
 
 ECHO View the running containers on a cluster - started
-ecs-cli compose --project-name  %CLUSTER_NAME% service ps --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME% > %CONTAINERS_INFO_FILE_NAME%
+ecs-cli compose --project-name  %APP_NAME% service ps --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME% > %CONTAINERS_INFO_FILE_NAME%
 ECHO View the running containers on a cluster - ended
 
 ECHO Fetch Containers info - started
@@ -108,15 +157,15 @@ FOR /f "tokens=1,2 delims==" %%A in (%CONTAINERS_INFO_PROCESSED_FILE_NAME%) do (
 ECHO Fetch Containers info - ended
 
 REM ECHO View the container logs - started
-REM ecs-cli logs --task-id %FOUND_TASK_IDS% --follow --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
+REM ecs-cli logs --task-id %FOUND_TASK_IDS% --follow --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME%
 REM ECHO View the container logs - end
 
 ECHO Scale the tasks on the cluster - started
-ecs-cli compose --project-name %CLUSTER_NAME% service scale 2 --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
+ecs-cli compose --project-name %APP_NAME% service scale 2 --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME%
 ECHO Scale the tasks on the cluster - ended
 
 ECHO View the running containers on a cluster (after scale) - started
-ecs-cli compose --project-name  %CLUSTER_NAME% service ps --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME% > %CONTAINERS_INFO_FILE_NAME%
+ecs-cli compose --project-name  %APP_NAME% service ps --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME% > %CONTAINERS_INFO_FILE_NAME%
 ECHO View the running containers on a cluster (after scale) - end
 
 ECHO Fetch Containers info (after scale) - started
@@ -129,81 +178,20 @@ ECHO Fetch Containers info (after scale) - ended
 REM FOR %%A in (%FOUND_2_TASK_IDS%) do (
 REM    ECHO CURR_FOUND_TASK_ID '%%A'
 REM    ECHO View container logs - started
-REM    ecs-cli logs --task-id %CURR_FOUND_TASK_ID% --follow --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
+REM    ecs-cli logs --task-id %CURR_FOUND_TASK_ID% --follow --cluster-config %APP_NAME% --ecs-profile %PROFILE_NAME%
 REM    ECHO View container logs - end
 REM )
 
-ECHO Get Repository info - started
-aws ecr describe-repositories --repository-names %REPOSITORY_NAME% --region %REGION% > %REPOSITORY_INFO_FILE_NAME%
-ECHO Get Repository info - ended
+REM ================= 2nd part - end ==============================
 
-ECHO Fetch Repository info - started
-CALL node %MY_UTILS_PATH% --repository-info %REPOSITORY_INFO_FILE_NAME% %REPOSITORY_INFO_PROCESSED_FILE_NAME%
-FOR /f "tokens=1,2 delims==" %%A in (%REPOSITORY_INFO_PROCESSED_FILE_NAME%) do (
-    SET FOUND_%%A=%%B
-)
-ECHO Fetch Repository info - ended
 
-IF NOT DEFINED FOUND_REPOSITORY_URI (
-    ECHO Create repository - started
-    aws ecr create-repository --repository-name %REPOSITORY_NAME% --region %REGION% > %REPOSITORY_INFO_FILE_NAME%
-    ECHO Create repository - ended
-)
-
-ECHO Fetch Repository info (2nd time) - started
-CALL node %MY_UTILS_PATH% --repository-info %REPOSITORY_INFO_FILE_NAME% %REPOSITORY_INFO_PROCESSED_FILE_NAME%
-FOR /f "tokens=1,2 delims==" %%A in (%REPOSITORY_INFO_PROCESSED_FILE_NAME%) do (
-    SET FOUND_%%A=%%B
-)
-ECHO Fetch Repository info (2nd time) - ended
-
-REM ECHO get Task Definition - started
-REM aws ecs describe-task-definition --task-definition %CLUSTER_NAME% --query taskDefinition --region %REGION%
-REM ECHO get Task Definition - ended
+REM ================= 3rd part - start ==============================
 
 ECHO Set GitHub params - started
-SET ECR_REPOSITORY=%CLUSTER_NAME%
-SET SERVICE_NAME=%CLUSTER_NAME%
-REM TODO: aws-deployment-test ???
-SET CONTAINER_NAME=aws-deployment-test
 CALL node %MY_UTILS_PATH% --github-params %GITHUB_PARAMS_TEMPLATE_FILE_NAME% %GITHUB_PARAMS_FILE_NAME% %REGION% %ECR_REPOSITORY% %FOUND_TASK_DEFINITION% %CONTAINER_NAME% %SERVICE_NAME% %CLUSTER_NAME%
 ECHO Set GitHub params - ended
 
-REM ECHO Clear all resources - started
-REM ecs-cli compose --project-name %CLUSTER_NAME% service down --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
-REM ecs-cli down --force --cluster-config %CLUSTER_NAME% --ecs-profile %PROFILE_NAME%
-REM ??? aws iam --region %REGION% delete-role --role-name %ROLE_NAME%
-REM ??? aws ecr delete-repository --repository-name %REPOSITORY_NAME% --region %REGION% > %DELETED_REPOSITORY_INFO_FILE_NAME%
-REM ECHO Clear all resources - ended
-
-REM ===============================================
-
-ECHO Fetch Repository info - started
-CALL node %MY_UTILS_PATH% --repository-info %REPOSITORY_INFO_FILE_NAME% %REPOSITORY_INFO_PROCESSED_FILE_NAME%
-FOR /f "tokens=1,2 delims==" %%A in (%REPOSITORY_INFO_PROCESSED_FILE_NAME%) do (
-    SET FOUND_%%A=%%B
-)
-ECHO Fetch Repository info - ended
-
-ECHO REPOSITORY_URI: '%FOUND_REPOSITORY_URI%'
-
-ECHO Build - started
-docker build -t %FOUND_REPOSITORY_URI% .
-ECHO Build - ended
-
-ECHO Push - started
-docker push %FOUND_REPOSITORY_URI%
-ECHO Push - ended
-
-REM ===============================================
-
-REM TODO: add tag!
-REM docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG .
-REM docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-REM echo "::set-output name=image::s
-
-
-REM =================================================
+REM ================= 3rd part - end ==============================
 
 :END
 PAUSE
